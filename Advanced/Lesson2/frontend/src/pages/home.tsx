20a074e196a91e495d243577978009200f8c6086
Mulander <39454158+Mulander-J@users.noreply.github.com>
2022-05-31 22:53:12 +0800

    feat: code

--- Advanced/Lesson2/frontend/src/pages/home.tsx
+++ Advanced/Lesson2/frontend/src/pages/home.tsx
@@ -10,7 +10,7 @@ export default function PageHome() {
 
   return (
     <div className="grid text-center justify-center items-center h-full w-full">
-      <h1 className="text-3xl">Uptown DAO</h1>      
+      <h1 className="text-3xl font-phattype">Uptown DAO</h1>      
       <p className="text-lg">Welcome to Uptown DAO.<br/>Every menber has the right to vote for the proposal of canister's action.<br/>Come & enjoy the fun.</p>      
       <div className="bg-purple-500 hover:bg-purple-600 active:bg-purple-700 leading-5 w-40 mx-auto mt-4 px-5 py-2 text-sm rounded-full font-semibold text-white cursor-pointer" onClick={clickStart}>Start</div>
     </div>
